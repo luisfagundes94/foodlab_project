3a94ab4a73ad6755d8c5c781bd45596eea5a38f3
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2023-01-28 12:15:12 -0300

    removing unused imports from RecipeSteps.kt

--- features/recipes/src/main/java/com/luisfagundes/detail/RecipeSteps.kt
+++ features/recipes/src/main/java/com/luisfagundes/detail/RecipeSteps.kt
@@ -4,13 +4,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import com.luisfagundes.domain.models.Step
